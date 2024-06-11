05210de8c2536d8c4bf9ca74d63636201b710176
Victor Tedesco <victor.augusto.tedesco@gmail.com>
2024-06-11 18:15:58 -0300

    Add file import functionality and update user instructions

--- Program.cs
+++ Program.cs
@@ -6,27 +6,82 @@ namespace Employment
     internal class Program
     {
         private readonly static InputReader _inputReader = new();
+        private static bool isValidEntries = true;
         private static bool isRegisteringEmployees = true;
 
         static void Main()
         {
+            Console.Clear();
             Console.InputEncoding = System.Text.Encoding.UTF8;
             Console.OutputEncoding = System.Text.Encoding.UTF8;
+            var importFromFile = _inputReader.ReadString("Deseja importar funcionários de um arquivo? (Y/N):").ToLower()[0] == 'y';
 
-            while (isRegisteringEmployees)
+            if (importFromFile)
             {
-                RegisterEmployee();
+                ImportFromFile();
+            }
+            else
+            {
+                while (isRegisteringEmployees)
+                {
+                    RegisterEmployee();
+                }
             }
 
-            DisplayEmployees();
-
-            bool writeToFile = _inputReader.ReadString("Deseja escrever esse relátório em um arquivo .csv? (Y/N):").ToLower()[0] == 'y';
-            if (writeToFile)
+            if (isValidEntries)
             {
-                WriteToFile();
+                DisplayEmployees();
+
+                var writeToFile = _inputReader.ReadString("Deseja escrever esse relátório em um arquivo .csv? (Y/N):").ToLower()[0] == 'y';
+                if (writeToFile)
+                {
+                    WriteToFile();
+                }
             }
 
             Console.ReadLine();
+        }
+
+        private static void ImportFromFile()
+        {
+            string path = "funcionarios.txt";
+            // Formato correto para o arquivo: Nome; Horas; ValorPorHora; TaxaAdicional.
+            // Se taxa adicional for 0, o funcionário não é terceirizado.
+            if (File.Exists(path))
+            {
+                using StreamReader streamReader = new(path);
+                string line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    Employee.CurrentId++;
+                    var data = line.Split(';');
+                    
+                    try
+                    {
+                        string name = data[0];
+                        uint hours = uint.Parse(data[1]);
+                        double valuePerHour = double.Parse(data[2].Replace(",", "."));
+                        double additionalCharge = double.Parse(data[3].Replace(",", "."));
+                        if (additionalCharge == 0)
+                        {
+                            Employee.Employees.Add(new Employee(name, hours, valuePerHour));
+                        }
+                        else
+                        {
+                            Employee.Employees.Add(new OutSourcedEmployee(name, hours, valuePerHour, additionalCharge));
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"Erro ao importar funcionário: {e.Message}");
+                    }
+                }
+            } 
+            else
+            {
+                Console.WriteLine("Arquivo \"funcionarios.txt\" não encontrado.");
+                isValidEntries = false;
+            }
         }
 
         private static void RegisterEmployee()
@@ -64,12 +119,12 @@ namespace Employment
         {
             string path = "relatorio.csv";
 
-            using StreamWriter sw = new(path, false);
+            using StreamWriter streamWriter = new(path, false);
 
-            sw.WriteLine("Nome, Pagamento");
+            streamWriter.WriteLine("Nome, Pagamento");
             foreach (var employee in Employee.Employees)
             {
-                sw.WriteLine($"{employee.Name}, {employee.GetPayment():0.00}");
+                streamWriter.WriteLine($"{employee.Name}, {employee.GetPayment():0.00}");
             }
             Console.WriteLine($"\nRelatório escrito em {Path.GetFullPath(path)}");
         }
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Esse projeto é uma aplicação para console usando .NET
 
 ## Como executar
-Clone esse repositório e use o comando `dotnet run` com o .NET 8 ou superior.
+Clone esse repositório e use o comando `dotnet run` com o .NET 8.
